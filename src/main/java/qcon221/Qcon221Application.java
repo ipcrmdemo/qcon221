b5db756554df271031ea6489168bba6450da2fee
Matt Cadorette <mcadorette@gmail.com>
2018-11-06 18:36:38 -0800

    Initial commit from Atomist

---
+++ src/main/java/qcon221/Qcon221Application.java
@@ -0,0 +1,12 @@
+package qcon221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qcon221Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qcon221Application.class, args);
+	}
+}
